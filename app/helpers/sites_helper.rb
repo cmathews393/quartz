cb6c4afbe4e6689206cb95da253eaeb21235f370
0xChloe <chloe@catastrophesoftware.com>
2026-07-18 04:08:53 +0000

    feat: create helper functions to check liveness

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -1,6 +1,35 @@
+require "net/http"
 module SitesHelper
   def device_for_site(site)
-    level_client.get_device(site.level_id)
+    return unless ENV["LEVEL_API_KEY"] && site.level_id
+
+    begin
+      level_client.get_device(site.level_id)["online"]
+    rescue
+      nil
+    end
+  end
+
+  def reachable(site)
+    begin
+      response = Net::HTTP.get_response(URI(site.url))
+      if site.acceptable_response.present?
+        acceptable_codes = site.acceptable_response.is_a?(Array) ? site.acceptable_response : site.acceptable_response.split(",")
+        acceptable_codes = acceptable_codes.map(&:to_s).map(&:strip)
+      else
+        acceptable_codes = [ "200" ]
+      end
+
+      { ok: acceptable_codes.include?(response.code), code: response.code }
+    rescue Errno::ECONNREFUSED
+      { ok: false, error: "connection_refused" }
+    rescue Errno::EHOSTUNREACH
+      { ok: false, error: "host_unreachable" }
+    rescue Timeout::Error
+      { ok: false, error: "timeout" }
+    rescue Socket::ResolutionError
+      { ok: false, error: "unresolved_addr" }
+    end
   end
 
   private
